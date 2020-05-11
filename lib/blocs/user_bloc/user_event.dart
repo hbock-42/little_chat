b89cc28fccbcba6aee66ea4e41cedc544888f032
Hugo Bock <h.bock@easyrecrue.com>
2020-05-11 20:00:06 +0000

    start user bloc

--- lib/blocs/user_bloc/user_event.dart
+++ lib/blocs/user_bloc/user_event.dart
@@ -3,3 +3,13 @@ part of 'user_bloc.dart';
 abstract class UserEvent extends Equatable {
   const UserEvent();
 }
+
+class TrySignInEvent extends UserEvent {
+  final String email;
+  final String password;
+
+  const TrySignInEvent(this.email, this.password);
+
+  @override
+  List<Object> get props => [email, password];
+}
--- lib/blocs/user_bloc/user_state.dart
+++ lib/blocs/user_bloc/user_state.dart
@@ -2,7 +2,11 @@ part of 'user_bloc.dart';
 
 abstract class UserState extends Equatable {
   const UserState();
+  @override
+  List<Object> get props => [];
 }
+
+class NotLogged extends UserState {}
 
 class UserLogged extends UserState {
   final FirebaseUser user;
